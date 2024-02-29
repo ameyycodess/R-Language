d7200b44fe0e320e277debe50be0f27aafd22070
Amey Murkar <146580786+ameyycodess@users.noreply.github.com>
2024-02-29 11:36:44 +0530

    Add files via upload

---
+++ R pracs amey/prac1.R
@@ -0,0 +1,11 @@
+list_data <- list("BMW", "Audi", c(21,32,11), TRUE, 51.23, 119.1)
+print(list_data)
+
+my_array <- c("Supra","Mustang","BMW","Audi","Jaguar")
+print(my_array)
+
+stud_data <- data.frame(
+   stud_id = c (18001:18003), 
+   stud_name = c("Amey","Piyush","Vedant"),
+   stud_cgpa = c(8.8,8.5,9.1))
+print(stud_data)
---
+++ R pracs amey/prac2.R
@@ -0,0 +1,13 @@
+mat1 <- matrix(c(10,13,22,15),nrow = 2)
+print(mat1)
+
+mat2 <- matrix(c(23,34,67,13),nrow = 2)
+print(mat2)
+
+print(mat1 + mat2)
+
+print(mat1 * mat2)
+
+print(inv(mat1))
+
+print(t(mat1))
